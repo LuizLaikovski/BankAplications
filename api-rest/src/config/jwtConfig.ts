a8a7d2f07c49b531509b4fbb3e1f276556496fd2
Luiz Laikovski <luizlaikovski@gmail.com>
2025-12-09 20:56:58 -0300

    arrumando versooes do jwt

--- api-rest/src/config/jwtConfig.ts
+++ api-rest/src/config/jwtConfig.ts
@@ -1,4 +1,13 @@
-export const jwtConfig = {
-    secret: process.env.SECRET_KEY || "",
+export interface JwtConfig {
+    secret: string;
+    expiresIn: string;
+}
+
+if (!process.env.SECRET_KEY) {
+    throw new Error("SECRET_KEY não definida no .env");
+}
+
+export const jwtConfig: JwtConfig = {
+    secret: process.env.SECRET_KEY as string,
     expiresIn: '1d'
 }
